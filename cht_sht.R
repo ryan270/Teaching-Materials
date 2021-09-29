63b73b9710734b00e44a769c694d0ace556a6ff4
ry <ryan.fergusson.270@gmail.com>
2021-09-29 13:05:11 -0700

    Linted first half of cht_sht

--- cht_sht.R
+++ cht_sht.R
@@ -10,28 +10,28 @@
 3 * 3
 
 # a string
-'three'
+"three"
 # Strings are always surrounded by quotes. No numerical properties
-'three' * 'three' # Error
+"three" * "three" # Error
 
 
-# Logic & Conditionals
-3 > 4 # Logical statement. Asking whether 3 is greater than 4. It's FALSE
+# Logic and Conditionals
+3 > 4 # Logical statement. Asking whether 3 is greater than 4. It"s FALSE
 4 == 4 # TRUE
-'three' == 'three' # works for strings too
+"three" == "three" # works for strings too
 5 != 4 # TRUE
-'three' == 3 #F ALSE. strings have no numerical properties
+"three" == 3 #F ALSE. strings have no numerical properties
 
 
-# Vectors & Variables
-c(8,1,9,-1,5, 6) # numerical vector
-c('red', 'orange', 'yellow', 'green', 'blue') # character vector
+# Vectors and Variables
+c(8,1,9, -1, 5, 6) # numerical vector
+c("red", "orange", "yellow", "green", "blue") # character vector
 
-# numerical vector stored as variable (or object) called 'x'
-x <- c(8,1,9,-1,5, 6) 
+# numerical vector stored as variable (or object) called "x"
+x <- c(8, 1, 9, -1, 5, 6)
 
-# character vector stored as 'y'
-y <- c('red', 'orange', 'yellow', 'green', 'blue') 
+# character vector stored as "y"
+y <- c("red", "orange", "yellow", "green", "blue")
 
 # Use square brackets [] to navigate vectors
 x[1] # Navigates to first element of the vector
@@ -42,49 +42,49 @@ y[3] # Third element
 # Use square brackets [] to navigate two-dimensional data as well
 # [row, column]
 mtx <- matrix(x, nrow = 3, ncol = 2) # a matrix can only be numeric
-mtx[1,1] # returns entry in the first row and first column
-mtx[1,2] # returns entry in the first row and second column
-mtx[3,3] # returns entry in the third row and third column
-mtx[2,] # returns all entries in second row
+mtx[1, 1] # returns entry in the first row and first column
+mtx[1, 2] # returns entry in the first row and second column
+mtx[3, 3] # returns entry in the third row and third column
+mtx[2, ] # returns all entries in second row
 
 # Data Frames (IMPORTANT)
-# Data frames are the most common data type you'll be working with
-our_group <- data.frame(first_names = c('ryan', 'vu', 'kelly', 'daniella'),
+# Data frames are the most common data type you"ll be working with
+our_group <- data.frame(first_names = c("ryan", "vu", "kelly", "daniella"),
                         zip_codes = c(94933, 95823, 92395, 79912),
-                        fav_music = c('vogue', 'k-pop', 'rap', 'french pop'))
+                        fav_music = c("vogue", "k-pop", "rap", "french pop"))
 # Use $ to navigate data frame columns
 our_group$first_names  # Returns all entries in first_name column
 our_group$first_names[2] # Returns second entry in first_name column
-our_group[4,4] # Returns entry in the 4th row and 4th column
+our_group[4, 4] # Returns entry in the 4th row and 4th column
 
 
 
 #-------------------------------------#
 ##WORKSHEET 3: Functions
 # Function format: function(object)
-z <- c(7,8,9)
-sum(z) # the function is 'sum' the object is 'z'
-mean(z) # returns the mean of the object 'z'
+z <- c(7, 8, 9)
+sum(z) # the function is "sum" the object is "z"
+mean(z) # returns the mean of the object "z"
 sd(z) # returns the standard deviation of the object
 
 #function inside of a function: fx2(fx1(object))
 # The output of fx1 is the object of fx2
-diff(c(mean(z), sd(z))) #returns the difference of the mean and standard deviation
+diff(c(mean(z), sd(z))) #returns the diff of the mean and standard deviation
 
 
 
 #-------------------------------------#
 ##WORKSHEET 4: Importing & Exporting Data
-# Example file pathway: '/User/your_username/Downloads/'
+# Example file pathway: "/User/your_username/Downloads/"
 
 # Loading Data
-setwd('/User/your_username/Downloads/') # sets working directory (folder)
+setwd("/User/your_username/Downloads/") # sets working directory (folder)
 getwd() # gets working directory
-your_data <- read.csv('file_name.txt') # Only works if file is in directory above
+your_data <- read.csv("file_name.txt")
 
 # Exporting Data
-a <- seq(1,71,3) # goes from 1 to 71 in increments of 3. So 1,4,7,10... to 71
-write(a, file = 'our_sequence.txt') #Exports 'a' as a file to our working directory
+a <- seq(1, 71, 3) # goes from 1 to 71 in increments of 3. So 1,4,7,10... to 71
+write(a, file = "our_sequence.txt") #Exports "a" as a file
 
 
 
@@ -97,7 +97,7 @@ write(a, file = 'our_sequence.txt') #Exports 'a' as a file to our working direct
 # and again when i = 3. Each time it runs, it will run the same function the same
 # way. The function we want repeated goes between the curly braces.
 for(i in 1:3){
-    print('hello world')
+    print("hello world")
 }
 
 # Return different outputs
@@ -111,7 +111,7 @@ for(nu in 1:3){
 }
 
 # For loops work on strings as well
-color <- c('red', 'orange', 'yellow', 'green', 'blue')
+color <- c("red", "orange", "yellow", "green", "blue")
 for(i in color){
     print(i)
 }
@@ -125,7 +125,7 @@ for(i in 1:4){
 for(i in 1:4){
     our_group$first_names[i]
     our_group$zip_codes[i]  - 9000
-    print('if each of us had a smaller zip code')
+    print("if each of us had a smaller zip code")
 }
 
 
@@ -135,52 +135,52 @@ for(i in 1:4){
 # If statements perform certain function only if a condition is met
 # Only prints when condition is met
 if(1==1){
-    print('obviously')
+    print("obviously")
 }
 
 # No output
 if(1==42){
-    print('obviously')
+    print("obviously")
 }
 
 # else statements perform different functions for when the condition is false
 if(1==42){
-    print('obviously')
+    print("obviously")
 } else {
-    print('apparently not')
+    print("apparently not")
 }
 
-# you can also use 'else if' statements to string together multiple conditions
+# you can also use "else if" statements to string together multiple conditions
 if(1==42){
-    print('condition 1 is true')
+    print("condition 1 is true")
 } else if (20 > 30){
-    print('condition 2 is true')
+    print("condition 2 is true")
 } else if (100 > 12){
-    print('condition 3 is true')
+    print("condition 3 is true")
 }
 
 # If statements are VERY useful when used within for loops
 for(i in 1:length(our_group)){
     if(our_group$zip_codes[i] == 95823){
-        print("Vu's hometown")
+        print("Vu"s hometown")
     }
 }
 
-# Let's add a new column to the 'our_group' dataframe that has the state for each
+# Let"s add a new column to the "our_group" dataframe that has the state for each
 # zip code.
 our_group$state <- 0
 
 for(i in 1:length(our_group)){
     if(our_group$zip_codes[i] == 94933){
-        our_group$state[i] <- 'California'
+        our_group$state[i] <- "California"
     } else if (our_group$zip_codes[i] == 95823){
-        our_group$state[i] <- 'California'
+        our_group$state[i] <- "California"
         # hint: I made all the code below by copy and pasting the two lines
         # above and just changing the zip code & state
     } else if (our_group$zip_codes[i] == 92395){
-        our_group$state[i] <- 'California'
+        our_group$state[i] <- "California"
     } else if (our_group$zip_codes[i] == 79912){
-        our_group$state[i] <- 'Texas'
+        our_group$state[i] <- "Texas"
     }
 }
 
@@ -190,8 +190,8 @@ for(i in 1:length(our_group)){
 ##WORKSHEET 7: Plots
 # Using ggplot is much more versatile than using the base R plot
 # There are too many customization option for ggplot for me to try explaining
-# with the time I've been given.
-# With time & practice, I'm certain you'll start understanding them :)
+# with the time I"ve been given.
+# With time & practice, I"m certain you"ll start understanding them :)
 
 # to use ggplot, we have to install and load tidyverse
 library(tidyverse) # you can load tidyverse at the beginning of the script also
@@ -206,30 +206,30 @@ ggplot(data = our_group, aes(x=first_names, y=zip_codes))+
     geom_col() #column (or bar) plot
 
 
-# Let's change the 'fill' of the plot according to the state column
+# Let"s change the "fill" of the plot according to the state column
 ggplot(data = our_group, aes(x=first_names, y=zip_codes, fill = state))+
     geom_col()
 
 
-# Let's rename all of the axis titles, and give this plot a title
+# Let"s rename all of the axis titles, and give this plot a title
 ggplot(data = our_group, aes(x=first_names, y=zip_codes, fill = state))+
     geom_col()+
-    labs(x='Names', y='Zip Codes', title = 'Group Residence',
-         fill = 'Home State')
+    labs(x="Names", y="Zip Codes", title = "Group Residence",
+         fill = "Home State")
 
 
-# Finally, let's resize all of the axis title to make them larger
+# Finally, let"s resize all of the axis title to make them larger
 ggplot(data = our_group, aes(x=first_names, y=zip_codes, fill = state))+
     geom_col()+
-    labs(x='Names', y='Zip Codes', title = 'Group Residence',
-         fill = 'Home State')+
+    labs(x="Names", y="Zip Codes", title = "Group Residence",
+         fill = "Home State")+
     theme(axis.title.x = element_text(size = 16),
           axis.title.y = element_text(size = 16),
-          plot.title = element_text(size = 20, face = 'bold'),
+          plot.title = element_text(size = 20, face = "bold"),
           legend.title = element_text(size = 14))
 
 # I could go on to change literally every element of this plot using ggplot
-# Save this cheat sheet an use it any time you're stuck on coding problems in
+# Save this cheat sheet an use it any time you"re stuck on coding problems in
 # the future :)
 
 # ry
